08a979765836fb4edd391e28aa080d873786a3c6
Fernando Figueroa <fernandofigueroa@MacBook-Pro-de-Fernando.local>
2024-03-06 22:03:47 -0600

    New intermediate functions, general validation and documenting

--- main.py
+++ main.py
@@ -1,15 +1,20 @@
 from DNAToolkit import *
 
-# Validate nucleotides
+# Toolkit Validations
 rand = 'ACCTGSEEF'
 print(validate_sequence(rand))
 rand = 'TtgAC'
 print(validate_sequence(rand))
-
 print(random_nucs(33))
-
 print(nuc_frequency(random_nucs(18)))
-
 print(nuc_freq_coll(random_nucs(24)))
-
 print(nuc_frequency('GTaadcFF'))
+print(transcription_seq('GTACCAGT'))
+
+# main
+rndSEQ = validate_sequence(random_nucs(100))
+print(reverse_complement(rndSEQ))
+pretty_helix(rndSEQ)
+print(gc_content(rndSEQ))
+print(gc_subcontent(rndSEQ, 10)) 
+
