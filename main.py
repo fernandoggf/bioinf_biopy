e21079d453022d8dac7079e7e0229af1547eadca
Fernando Figueroa <fernandofigueroa@MacBook-Pro-de-Fernando.local>
2024-03-09 14:03:57 -0600

    usage of the toolkit without validate the firsts functions

--- main.py
+++ main.py
@@ -1,16 +1,5 @@
 from DNAToolkit import *
 
-# Toolkit Validations
-# rand = 'ACCTGSEEF'
-# print(validate_sequence(rand))
-# rand = 'TtgAC'
-# print(validate_sequence(rand))
-# print(random_nucs(33))
-# print(nuc_frequency(random_nucs(18)))
-# print(nuc_freq_coll(random_nucs(24)))
-# print(nuc_frequency('GTaadcFF'))
-# print(transcription_seq('GTACCAGT'))
-
 # main
 rndSEQ = validate_sequence('ATAAGCATTCTCTGATGAAGCTTTGCGCGGTCTTGCGTACGGGAGTAGCACAGCC')
 print(reverse_complement(rndSEQ))
